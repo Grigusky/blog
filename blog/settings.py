79f1e78ebdeb99640eebbb0fcae79034818a9824
grégoire meunier <gregoire.meunier@live.fr>
2021-12-04 00:26:00 +0100

    I'm an idiot

--- blog/settings.py
+++ blog/settings.py
@@ -149,7 +149,7 @@ STATICFILES_DIRS = (
 )
 
 #  Add configuration for static files storage using whitenoise
-STATICFILES_STORAGE = 'whitenoise.django.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
