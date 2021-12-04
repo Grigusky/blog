d0bf80b32ce8b15236783dbd7a3b5efb4762e659
grégoire meunier <gregoire.meunier@live.fr>
2021-12-04 18:14:08 +0100

    add media part

--- page/urls.py
+++ page/urls.py
@@ -1,8 +1,11 @@
+from django.conf.urls.static import static
 from django.urls import path
+
+from blog import settings
 from .views import list_article, get_article
 
 urlpatterns = [
     path('', list_article, name="list_article"),
     path('category/<str:category>/', list_article, name="list_article_category"),
     path('article/<str:slug>', get_article, name="get_article"),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
